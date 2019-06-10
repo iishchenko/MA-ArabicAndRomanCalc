99d82e64d620d409ed212cf6020df6c05eb62d2c
iishchenko <irynaira@gmail.com>
2019-06-10 16:21:19 +0200

    fixed main

--- ArabicAndRomanCalc/src/org/ira/calculator/Application.java
+++ ArabicAndRomanCalc/src/org/ira/calculator/Application.java
@@ -4,22 +4,45 @@ import java.util.Scanner;
 
 public class Application {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
 
-        System.out.print("Input first number: ");
-        int num1 = in.nextInt();
+    System.out.print("Input first number: ");
+    String numInput1 = in.next();
 
-        System.out.print("Input second number: ");
-        int num2 = in.nextInt();
+    System.out.print("Input second number: ");
+    String numInput2 = in.next();
+    int num1 = getNumber(numInput1);
+    int num2 = getNumber(numInput2);
 
-        System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+    calculate(num1, num2);
+  }
 
-        System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+  private static int getNumber(String inputNumber){
+    // receives the input text, and tries to convert it in an arabic number
+    int num;
+    try {
+      //     try to convert the input Strings to integers, if there are not arabic numbers, it will fail
+      num = Integer.valueOf(inputNumber);
 
-        System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
-
-        System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+    } catch( NumberFormatException exception ) {
+      //     if an exception is captured, that means that they were roman numbers
+      RomanNumeral num1tr = new RomanNumeral(inputNumber);
+      num = num1tr.toInt();
     }
+    return num;
+  }
+
+  private static void calculate(int num1, int num2) {
+    //   do the basic calculator operations
+
+    System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+
+    System.out.println(num1 + " - " + num2 + " = " + (num1 - num2));
+
+    System.out.println(num1 + " x " + num2 + " = " + (num1 * num2));
+
+    System.out.println(num1 + " / " + num2 + " = " + (num1 / num2));
+  }
 
 }
